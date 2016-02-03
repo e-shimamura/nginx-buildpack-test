1d5a7cc677399aefde0227c642f3734aa9c7ed7a
root <root@ip-172-31-30-189.us-west-2.compute.internal>
2016-02-03 06:42:53 +0000

    first commit

---
+++ cf_spec/integration/deploy_staticfile_app_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'excon'
+
+describe 'deploy a staticfile app' do
+  let(:app) { Machete.deploy_app('staticfile_app') }
+  let(:browser) { Machete::Browser.new(app) }
+
+  after do
+    Machete::CF::DeleteApp.new.execute(app)
+  end
+
+  specify do
+    expect(app).to have_logged(/Buildpack version [\d\.]{5,}/)
+    expect(app).to be_running
+
+    browser.visit_path('/')
+    expect(browser).to have_body('This is an example app for Cloud Foundry that is only static HTML/JS/CSS assets.')
+
+    browser.visit_path('/fixture.json')
+    expect(browser.content_type).to eq('application/json')
+
+    response = Excon.get("http://#{browser.base_url}/lots_of.js", headers: {
+      'Accept-Encoding' => 'gzip'
+    })
+    expect(response.headers['Content-Encoding']).to eq('gzip')
+  end
+
+  context 'with a cached buildpack', :cached do
+    it 'logs the files it downloads' do
+      expect(app).to have_logged(/Downloaded \[file:\/\/.*\]/)
+    end
+
+    it 'does not call out over the internet' do
+      expect(app).to_not have_internet_traffic
+    end
+  end
+
+  context 'with a uncached buildpack', :uncached do
+    it 'logs the files it downloads' do
+      expect(app).to have_logged(/Downloaded \[https:\/\/.*\]/)
+    end
+  end
+end
